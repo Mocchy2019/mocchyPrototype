cdf6c3b207883ff3bdd286963569b184f718a5a3
Ariel Davis <ariel.z.davis@icloud.com>
2019-01-31 21:05:51 +0900

    Disable scroll when menu open

--- 団体紹介/index.js
+++ 団体紹介/index.js
@@ -1,11 +1,14 @@
 const topBarBurger = document.querySelector(".top-bar__burger");
 const topBar = document.querySelector(".top-bar");
 const topBarOpenClassName = "top-bar--open";
+const noScroll = "no-scroll";
 let navVisible = false;
 topBarBurger.onclick = () => {
   if (navVisible) {
+    document.documentElement.classList.remove(noScroll);
     topBar.classList.remove(topBarOpenClassName);
   } else {
+    document.documentElement.classList.add(noScroll);
     topBar.classList.add(topBarOpenClassName);
   }
   navVisible = !navVisible;
