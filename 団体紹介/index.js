24d86811a4d06f7b6692de3eab27ca5dada18456
Ariel Davis <ariel.z.davis@icloud.com>
2019-01-31 16:29:29 +0900

    Start menu

--- 団体紹介/index.js
+++ 団体紹介/index.js
@@ -1,1 +1,12 @@
-console.log("hello")
+const topBarBurger = document.querySelector(".top-bar__burger");
+const topBarNav = document.querySelector(".top-bar__nav");
+const topBarNavOpenClassName = "top-bar__nav--open";
+let navVisible = false;
+topBarBurger.onclick = () => {
+  if (navVisible) {
+    topBarNav.classList.remove(topBarNavOpenClassName);
+  } else {
+    topBarNav.classList.add(topBarNavOpenClassName);
+  }
+  navVisible = !navVisible;
+}
