ca902fc5ab489543de99e7bee88c3d47c8c287fa
Ariel Davis <ariel.z.davis@icloud.com>
2019-01-31 21:06:03 +0900

    Shorten

--- 団体紹介/index.js
+++ 団体紹介/index.js
@@ -1,15 +1,15 @@
 const topBarBurger = document.querySelector(".top-bar__burger");
 const topBar = document.querySelector(".top-bar");
-const topBarOpenClassName = "top-bar--open";
+const topBarOpen = "top-bar--open";
 const noScroll = "no-scroll";
 let navVisible = false;
 topBarBurger.onclick = () => {
   if (navVisible) {
     document.documentElement.classList.remove(noScroll);
-    topBar.classList.remove(topBarOpenClassName);
+    topBar.classList.remove(topBarOpen);
   } else {
     document.documentElement.classList.add(noScroll);
-    topBar.classList.add(topBarOpenClassName);
+    topBar.classList.add(topBarOpen);
   }
   navVisible = !navVisible;
 }
